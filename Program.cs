99db29b6b8653708002a1f6256c2d1f7446ca6f1
Dmitriy <79133850099@yandex.ru>
2024-01-08 21:55:19 +0700

    Task 48

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿﻿/*
+Задача 48: Задайте двумерный массив размера m на n, 
+каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5
+
+*/
+// Двумерный массив
+
+// Метод для создания массива
+// Двумерный массив = matrix
+// m - количество строчек, n - количество столбцов
+Console.Write("Введите количество строчек : ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов : ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] resMatrix = GetMatrix(rows, columns);
+// rows = 3 columns = 4 => таблица из 3-х строк и 4 -х столбцов, элемент: число от min до max включительно
+PrintMatrix(resMatrix); // PrintMatrix(матрица)
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m,n]; // Таблица из m - строк и n - столбцов
+    for (int i = 0; i < matrix.GetLength(0); i++) // Цикл по строчкам, i < m
+    {
+       // i, j, m, k
+       for (int j = 0; j < matrix.GetLength(1); j++) // Цикл по столбцам, j < n
+       {
+        matrix[i, j] = i + j;
+       } 
+    }
+    return matrix;
+}
+// Метод, который печатает массив
+void PrintMatrix(int[,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i++) // строчки
+    {
+       for (int j = 0; j < array2D.GetLength(1); j++) // столбцы
+       {
+        Console.Write(array2D[i, j] + "\t"); // "\t" = Tab = 4 пробела между элементами
+       } 
+       Console.WriteLine();
+    }
+
+}
